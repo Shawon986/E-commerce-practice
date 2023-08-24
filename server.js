67234b7b2ed3d4603a688079d0362b59cc1d378a
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-08-24 10:43:33 +0600

    create user done

--- server.js
+++ server.js
@@ -17,3 +17,33 @@ app.listen(port,()=>{
 app.get("/",(req,res)=>{
     res.json({message:"Welcome to E-Shopping"})
 }) 
+
+
+const userSchema = new mongoose.Schema({
+        name :String,
+        email: String,
+        age: Number,
+        password: String,
+
+})
+
+const User = mongoose.model("User",userSchema)
+
+//! Create a user API
+app.post("/create",async(req,res)=>{
+    try {
+        const userObj={
+            name:req.body.name,
+            email:req.body.email,
+            age:req.body.age,
+            password:req.body.password
+        }
+        const user = new User(userObj)
+        res.status(201).json(user)
+        await user.save()
+    } catch (error) {
+       res.status(400).json({message:"Something went wrong in this server" })
+    }
+   
+})
+
